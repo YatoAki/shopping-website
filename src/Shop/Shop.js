19af1d7d4f85744aa97970f7299354302dc34c6d
Yato <yatoakii@gmail.com>
2022-09-17 12:40:14 +0630

    Added required states

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Shop.css"
 import Nav from "./Components/Nav/Nav.js"
 import Cart from "./Components/Cart/Cart.js"
 import ShoppingArea from "./Components/ShoppingArea/ShoppingArea.js"
 
 const Shop = ()=> {
+
+    const [currentArea, setCurrentArea] = useState("iPhone")
+
+    const navClicked = (area) => {
+        setCurrentArea(area)
+    }
 
     return(
         <div class="shop">
