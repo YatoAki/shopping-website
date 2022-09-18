d1767a0588c470924e2317a5b989a6a95067382f
Yato <yatoakii@gmail.com>
2022-09-18 10:52:59 +0630

    added shopping area iphone images

--- src/Shop/Components/ShoppingArea/ShoppingArea.js
+++ src/Shop/Components/ShoppingArea/ShoppingArea.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
+import "./ShoppingArea.css"
+import iphone7Plus from "./iphone/iPhone 7 Plus.png"
+import iphone11Pro from "./iphone/iPhone 11 Pro.png"
+import iphoneX from "./iphone/iPhone X.png"
+import iphoneXR from "./iphone/iPhone XR.png"
 
 const ShoppingArea = ({currentArea}) => {
-
+    
+    const [cards,setCards] = useState({
+        iphone7plus: iphone7Plus, 
+        iphone11Pro: iphone11Pro,
+        iphoneX: iphoneX,
+        iphoneXR: iphoneXR
+    })
+    console.log(cards)
     return(
-        <div>
-            THis is {currentArea}
+        <div className="shoppingArea">
+            {
+                Object.keys(cards).map( key => {
+                    return(
+                        <img src={cards[key]} alt={key} />
+                    )
+                })
+            }
         </div>
     )
 }
