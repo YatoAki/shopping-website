33de123845a7ad4eb4d80f32609f799b1512c49b
Yato <yatoakii@gmail.com>
2022-09-24 20:05:32 +0630

    Added HashRouter

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,18 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home/Home.js";
 import Shop from "./Shop/Shop.js";
 import About from "./About/About.js"
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter>
       <Routes>
-        <Route path="/shopping-website/" element={<Home />} />
-        <Route path="/shopping-website/home" element={<Home/>}/>
-        <Route path="/shopping-website/shop" element={<Shop />} />
-        <Route path="/shopping-website/about" element={<About />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/about" element={<About />}/>
       </Routes>
-    </BrowserRouter>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouteSwitch from "./RouteSwitch";
+import { HashRouter } from 'react-router-dom';
 import "./index.css"
-import { HashRouter } from 'react-router-dom'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,11 +12,12 @@ root.render(
     <header>
         <h1><a href="/">Apple Store</a></h1>
           <ul>
-          <li><a href="/shopping-website/home">Home</a></li>
-            <li><a href="/shopping-website/shop">Shop</a></li>
-            <li><a href="/shopping-website/about">About</a></li>
+          <li><a href="/home">Home</a></li>
+            <li><a href="/shop">Shop</a></li>
+            <li><a href="/about">About</a></li>
       </ul>
     </header>
+    
     <RouteSwitch />
     </HashRouter>
   </React.StrictMode>
